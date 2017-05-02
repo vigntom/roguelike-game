6efa6c7d0f1eacecdfdc5a8621895c392fc04139
Aleksandr I <vigntom@gmail.com>
2017-05-02 12:55:04 +0300

    Add player movements

--- src/js/main.js
+++ src/js/main.js
@@ -15,32 +15,80 @@ function debug (message) {
 
   const rogueLikeGame = (function rogueLikeGame () {
     const config = Object.freeze({
-      x: { name: 'cell-wall', output: ' ' },
-      p: { name: 'cell-player', output: ' ' },
-      e: { name: 'cell-enemy', output: ' ' },
-      w: { name: 'cell-weapon', output: ' ' },
-      h: { name: 'cell-health', output: ' ' },
-      0: { name: 'cell-space', output: ' ' }
+      wall: 'x',
+      player: 'p',
+      enemy: 'e',
+      weapon: 'w',
+      health: 'h',
+      space: '0',
+      x: 'cell-wall',
+      p: 'cell-player',
+      e: 'cell-enemy',
+      w: 'cell-weapon',
+      h: 'cell-health',
+      0: 'cell-space'
     })
 
     const init = [{
+      player: { x: 17, y: 7 },
+      enemy: [{ x: 21, y: 3 }, { x: 15, y: 7 }],
+      weapon: [{ x: 8, y: 3 }],
+      health: [{ x: 1, y: 2 }, { x: 18, y: 3 }, { x: 19, y: 8 }],
       dangeon: [
         'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-        'x0h000000xxx000000000h000x',
+        'x00000000xxx0000000000000x',
         'x00000000xxxxx00000000000x',
-        'x0000000000xxx0xx0000e000x',
+        'x0000000000xxx0xx00000000x',
         'x0000000000x000xxxxxxxxxxx',
         'x00000000xxx0xxxxxxxxxxxxx',
         'x000000000000000000000000x',
-        'x00000000x000000p000e0000x',
-        'x00w00000x000000000000h00x',
+        'x00000000x000000000000000x',
+        'x00000000x000000000000000x',
         'xxxxxxxxxxxxxxxxxxxxxxxxxx'
-      ]
+      ].map(str => Array.from(str))
     }]
 
     const actions = (function actions () {
+      const noop = () => [{}]
+      const keyMoveMap = new Map()
+
+      const moveUp = ({ x, y }) => ({ x, y: y - 1 })
+      const moveDown = ({ x, y }) => ({ x, y: y + 1 })
+      const moveLeft = ({ x, y }) => ({ x: x - 1, y })
+      const moveRight = ({ x, y }) => ({ x: x + 1, y })
+
+      keyMoveMap.set('ArrowUp', move(moveUp))
+      keyMoveMap.set('ArrowDown', move(moveDown))
+      keyMoveMap.set('ArrowLeft', move(moveLeft))
+      keyMoveMap.set('ArrowRight', move(moveRight))
+
+      keyMoveMap.set('w', move(moveUp))
+      keyMoveMap.set('s', move(moveDown))
+      keyMoveMap.set('a', move(moveLeft))
+      keyMoveMap.set('d', move(moveRight))
+
+      function move (step) {
+        return function (model) {
+          const next = Object.assign({}, model)
+          const dangeon = next.dangeon
+          const { x, y } = step(model.player)
+
+          if (dangeon[y][x] === config.space) {
+            next.player = { x, y }
+          }
+
+          return [next]
+        }
+      }
+
       return {
-        noop: () => [{}]
+        noop,
+        keyDown: key => {
+          const action = keyMoveMap.get(key)
+
+          if (action === undefined) { return noop }
+          return action
+        }
       }
     }())
 
@@ -48,17 +96,25 @@ function debug (message) {
       const h = React.createElement
       const renderer = el => ReactDOM.render(el, root$)
 
+      function keyDownHandler (ev) {
+        address(actions.keyDown(ev.key))
+      }
+
+      document.body.addEventListener('keydown', keyDownHandler)
+
       const app = (function app () {
         function App ({ dangeon }) {
-          return h('div', { className: 'game' },
+          return h('div', {
+            className: 'game'
+          },
             h('div', { className: 'dangeon' },
               dangeon.map((row, rowId) => (
                 h('div', { className: 'dangeon-row', key: rowId },
                   Array.from(row).map((cell, cellId) => (
                     h('div', {
-                      className: ['dangeon-cell', config[cell].name].join(' '),
+                      className: ['dangeon-cell', config[cell]].join(' '),
                       key: cellId
-                    }, config[cell].output)
+                    })
                   ))
                 )
               ))
@@ -70,14 +126,41 @@ function debug (message) {
       }())
 
       function reform (model) {
-        return model
+        function dangeonUpdate (position, value) {
+          if (Array.isArray(position)) {
+            return compose.apply(
+              undefined,
+              position.map(
+                pos => dangeonUpdate(pos, value)
+              )
+            )
+          }
+
+          return (dangeon) => {
+            const { x, y } = position
+            const data = dangeon.concat()
+            const row = data[y].concat()
+
+            row[x] = value
+            data[y] = row
+
+            return data
+          }
+        }
+
+        return compose(
+          dangeonUpdate(model.health, config.health),
+          dangeonUpdate(model.weapon, config.weapon),
+          dangeonUpdate(model.enemy, config.enemy),
+          dangeonUpdate(model.player, config.player)
+        )(model.dangeon)
       }
 
       return function render (model) {
-        const reflect = reform(model)
+        const dangeon = reform(model)
         const { App } = app
 
-        return renderer(h(App, reflect))
+        return renderer(h(App, { dangeon }))
       }
     }
 
@@ -92,7 +175,7 @@ function debug (message) {
 
     return WebApp.start({
       init,
-      render: view(compose(debug('And model is: '), WebApp.send, debug('Send a message: ')))
+      render: view(WebApp.send)
     })
   }())
 }(document.getElementById('app-container')))
